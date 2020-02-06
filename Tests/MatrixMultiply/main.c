66f51b705665d4b819ffe60c5134ed56db8e1353
benroywillis <benroywillis@gmail.com>
2020-02-05 19:30:14 -0700

    Added matrix multiply test. Needs to be reduced in size.

---
+++ Tests/MatrixMultiply/main.c
@@ -0,0 +1,60 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Backend/BackendTrace.h"
+
+#ifndef TRACING
+#define TRACING 0 
+#endif
+
+#define	SIZE 	512
+
+int main()
+{
+    int (*in0)[SIZE] = malloc( SIZE*sizeof(int[SIZE][SIZE]) ); 
+    int (*in1)[SIZE] = malloc( SIZE*sizeof(int[SIZE][SIZE]) ); 
+    int (*out)[SIZE] = malloc( SIZE*sizeof(int[SIZE][SIZE]) ); 
+
+    for( int i = 0; i < SIZE; i++ )
+    {
+	for( int j = 0; j < SIZE; j++ )
+	{
+	    in0[i][j] = rand();
+	    in1[i][j] = rand();
+	    out[i][j] = 0;
+	}
+    }
+
+#if TRACING
+    KernelEnter("MatrixMultiply,Outer");
+#endif
+    for( int i = 0; i < SIZE; i++ )
+    {
+#if TRACING
+    	KernelEnter("MatrixMultiply,Inner");
+#endif
+	for( int j = 0; j < SIZE; j++ )
+	{
+#if TRACING
+    	    KernelEnter("MatrixMultiply,Mul");
+#endif
+	    for( int k = 0; k < SIZE; k++ )
+	    {
+		out[i][j] += in0[i][k]*in1[k][j];
+	    }
+#if TRACING
+    	    KernelExit("MatrixMultiply,Mul");
+#endif
+	}
+#if TRACING
+    	KernelExit("MatrixMultiply,Inner");
+#endif
+    }
+#if TRACING
+    KernelExit("MatrixMultiply,Outer");
+#endif
+   
+    printf("Success.\n");
+    return 0;
+}
+
