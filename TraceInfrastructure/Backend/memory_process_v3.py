aa48e8091a829f11398a3d03ee17fe3f31bd0ffe
Liangliang Chang (Student) <lchang21@sundevils.asu.edu>
2020-02-24 10:16:50 -0700

    optimized speed

--- TraceInfrastructure/Backend/memory_process_v3.py
+++ TraceInfrastructure/Backend/memory_process_v3.py
@@ -1,7 +1,7 @@
-def firstStore(addr, t):
-    # end kernel if none at last
+def firstStore(addr,t):
+    # end kernel if none at last  str(t) + '-' + str(addr)   str(addr) + '-' + str(t)
     if t > 0:
-        virAddr[str(addr) + '-' + str(t)] = [addr, t, []]
+        virAddr[str(addr) + '-' + str(t)] = [addr,t,[]]
         ## todo new coming repeat address will replace old one here
         if addr not in deAlias:
             deAlias[addr] = str(addr) + '-' + str(t)  # latest store
@@ -16,8 +16,13 @@ def firstStore(addr, t):
             inputList.append(addr)
 
 
-def livingLoad(addr, t):
+def livingLoad(addr,t):
     virAddr[deAlias[addr]][2].append(t)
+
+
+def myFunc(e):
+    e = e.split('-')
+    return int(e[1])
 
 
 inputList = []
@@ -27,7 +32,7 @@ determineDir = {}
 aliveTbl = []
 deadTbl = []
 deAlias = {}
-file = open('fir_raw.trc', 'rt')
+file = open('raw.trc','rt')
 address = 0
 timing = -1
 output = []
@@ -38,35 +43,48 @@ for line in file.readlines():
     try:
         address = int(line[1])
     except:
-        print("wrong split at line number", timing)
+        print("wrong split at line number",timing)
         continue
 
     if address in deAlias and line[0] == "LoadAddress":
-        livingLoad(address, timing)
+        livingLoad(address,timing)
     elif line[0] == "LoadAddress":
         # start kernel
-        firstStore(address, -1)
+        firstStore(address,-1)
 
     elif line[0] == "StoreAddress":
-        firstStore(address, timing)
+        firstStore(address,timing)
 
     inputWorking.append(inputList.__len__())
-for time in range(0, timing):
+L = list(virAddr.keys())
+L.sort(key=myFunc)
+
+for time in range(0,timing):
     timeline = []
-    for addrPair in virAddr:
+    reviseDic = []
+    for addrPair in L:
+        if virAddr[addrPair][1] > time:
+            break
         if virAddr[addrPair][2]:
             if int(virAddr[addrPair][1]) < time < int(virAddr[addrPair][2][-1]):
                 timeline.append(addrPair)
+            elif time > int(virAddr[addrPair][2][-1]):
+                reviseDic.append(addrPair)
+        else:
+            outputList.append(addrPair)
+    for i in reviseDic:
+        L.remove(i)
     aliveTbl.append(timeline)
-    print("time progress:", time)
+    if time%100 == 0:
+        print("time progress:",time)
     output.append(timeline.__len__())
 
-f = open("./workingSet.txt", "w")
+f = open("./workingSet.txt","w")
 for line in output:
     f.writelines(str(line) + '\n')
 f.close()
 
-f = open("./inputWorkingSet.txt", "w")
+f = open("./inputWorkingSet.txt","w")
 for line in inputWorking:
     f.writelines(str(line) + '\n')
 f.close()
