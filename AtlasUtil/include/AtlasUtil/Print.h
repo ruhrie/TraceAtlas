a988945547dc73ce27d3bca4a92c18271540eece
Richard Uhrie <ruhrie@asu.edu>
2020-03-16 17:11:20 -0700

    migrated AtlasUtil to be header only

--- AtlasUtil/include/AtlasUtil/Print.h
+++ AtlasUtil/include/AtlasUtil/Print.h
@@ -1,6 +1,34 @@
+#pragma once
 #include <llvm/IR/Module.h>
+#include <llvm/IR/AssemblyAnnotationWriter.h>
+#include <iostream>
 
-void PrintVal(llvm::Value *val);
-void PrintVal(const llvm::Value *val);
-void PrintVal(llvm::Type *val);
-void PrintVal(llvm::Module *mod);
+inline void PrintVal(llvm::Value *val)
+{
+    std::string str;
+    llvm::raw_string_ostream rso(str);
+    val->print(rso);
+    std::cout << str << "\n";
+}
+
+inline void PrintVal(const llvm::Value *val)
+{
+    PrintVal(val);
+}
+
+inline void PrintVal(llvm::Module *mod)
+{
+    llvm::AssemblyAnnotationWriter *write = new llvm::AssemblyAnnotationWriter();
+    std::string str;
+    llvm::raw_string_ostream rso(str);
+    mod->print(rso, write);
+    std::cout << str << "\n";
+}
+
+inline void PrintVal(llvm::Type *val)
+{
+    std::string str;
+    llvm::raw_string_ostream rso(str);
+    val->print(rso);
+    std::cout << str << "\n";
+}
