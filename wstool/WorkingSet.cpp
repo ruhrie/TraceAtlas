5fa1015e9b3154f8a92b2de3006d1a3706581e35
lchang21 <lchang21@spade-11.fulton.asu.edu>
2020-03-25 04:37:49 -0700

    fix str integer casting

--- wstool/WorkingSet.cpp
+++ wstool/WorkingSet.cpp
@@ -5,24 +5,26 @@
 #include <set>
 #include <string>
 #include <sstream>
+#include<iostream>
 
 using namespace std;
 
 namespace WorkingSet
 {
-    int timing;
-    map<int,  string> deAlias;
-    map< string,  vector<int> > virAddr;
-    int inputSize;
-    void firstStore( string addr, int t, int op)
+    uint64_t timing = 0;
+    map< uint64_t,  string> deAlias;
+    map< string,  vector<uint64_t> > virAddr;
+    uint64_t inputSize;
+    void firstStore( string addr, uint64_t t, int op)
     {
-        int addrIndex = atoi(addr.c_str());
+        uint64_t addrIndex = stol(addr, 0, 0);
         string VirAddrIndex;
         stringstream ss;
         if (op > 0)
         {
-            // might problem int()           
-            vector<int> virAddrLine(2);
+            // might problem int()
+            //printf ("firstStore op1 \n");           
+            vector<uint64_t> virAddrLine(2);
             virAddrLine[0] = addrIndex;
             virAddrLine[1] = t;
             
@@ -46,9 +48,10 @@ namespace WorkingSet
         }           
         else
         {
+            //printf ("firstStore op0");
             if (deAlias.count(addrIndex) == 0)
             {
-                vector<int> virAddrLine(3);
+                vector<uint64_t> virAddrLine(3);
                 virAddrLine[0] = addrIndex;
                 virAddrLine[1] = -1;
                 virAddrLine[2] = t;
@@ -69,27 +72,32 @@ namespace WorkingSet
         }        
     }
 
-    void livingLoad(string addr, int t)
+    void livingLoad(string addr, uint64_t t)
     {
-        int addrIndex = atoi(addr.c_str());
+        uint64_t addrIndex = stol(addr, 0, 0);
         virAddr[deAlias[addrIndex]].push_back(t);
     }
         
     void Process( string &key,  string &value)
     {
-        int addrIndex = atoi(value.c_str());
+        long int addrIndex = stol(value, 0, 0);
         string address = value;
-        if ((deAlias.count(addrIndex) != 0) && (key == "LoadAddress"))
+        std::cout<<"key:"<< key << std::endl;
+        std::cout<<"value:"<< value << std::endl;
+        printf("addrIndex : %ld \n",addrIndex);
+        if ((deAlias.count(addrIndex) != 0) && (key == "WSLoadAddress"))
         {
+            printf ("11111 \n");
             livingLoad(address,timing);
         }                    
-        else if (key == "LoadAddress")
-        //start kernel
+        else if (key == "WSLoadAddress")
         {
+            printf ("2222 \n");
             firstStore(address,timing, -1);            
         }          
-        else if (key == "StoreAddress")
+        else if (key == "WSStoreAddress")
         {
+            printf ("3333 \n");
             firstStore(address,timing,1);
         }   
         timing++;         
