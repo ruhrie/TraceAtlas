9d8023a4aaf2e5d21efc2d2c453db2b97537b83d
Richard Uhrie <ruhrie@asu.edu>
2020-04-22 18:28:16 -0700

    deat try 2

--- deat/deat.cpp
+++ deat/deat.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <llvm/Support/CommandLine.h>
 #include <nlohmann/json.hpp>
+#include <queue>
 #include <set>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
@@ -16,20 +17,54 @@ cl::opt<std::string> OutputFilename("o", cl::desc("Specify output json"), cl::va
 cl::opt<std::string> TraceFilename(cl::Positional, cl::value_desc("Trace filename"), cl::Required);
 llvm::cl::opt<bool> noBar("nb", llvm::cl::desc("No progress bar"), llvm::cl::value_desc("No progress bar"));
 
-map<string, set<string>> parentMap;
+map<string, set<int64_t>> kernelMap; //dictionary mapping a name to a set of blocks
+string ck; ///string denoting the currently processing kernel
+map<int64_t, string> UIDMap; //map of a UID to a kernel name
+int64_t uidCounter = 0; //current uid counter. DO NOT TOUCH
+map<uint64_t, int64_t> writeMap; //dictionary mapping an address to the last uid to write to it
+map<int64_t, set<int64_t>> readMap; //dictionary mapping a uid to the uids it read frome
 
-map<string, set<int>> kernelMap;
+struct KernelStruct
+{
+    set<int64_t> blocks;
+    string name;
+    KernelStruct(set<int64_t> b, string n)
+    {
+        blocks = move(b);
+        name = move(n);
+    }
+};
+struct UIDStruct
+{
+    string name;
+    int64_t UID;
+    UIDStruct(string n)
+    {
+        UID = uidCounter++;
+        name = move(n);
+        UIDMap[UID] = name;
+    }
+};
 
-vector<string> kernels;
+queue<UIDStruct> kernelQueue;
+vector<KernelStruct> kernelSet;
 
-map<string, string> currentKernel;
-map<string, int64_t> lastblock;
-map<string, int64_t> currentUid;
-map<string, map<int, string>> kernelIdMap;
-map<string, int64_t> UID;
-map<string, map<uint64_t, int64_t>> writeMap;
+bool sizeSort(const KernelStruct &a, const KernelStruct &b)
+{
+    return a.blocks.size() < b.blocks.size();
+}
 
-map<string, map<int, set<int>>> consumerMap;
+string getKernel(int64_t block)
+{
+    for (auto k : kernelSet)
+    {
+        if (k.blocks.find(block) != k.blocks.end())
+        {
+            return k.name;
+        }
+    }
+    return "";
+}
 
 int64_t block;
 void Process(string &key, string &value)
@@ -37,77 +72,33 @@ void Process(string &key, string &value)
     if (key == "BBEnter")
     {
         block = stol(value, nullptr, 0);
-        for (const auto &kernel : kernels)
+        auto k = getKernel(block);
+        while (k != ck)
         {
-            if (kernelMap[kernel].find(block) != kernelMap[kernel].end())
+            //we have changed kernel contexts
+            if (kernelMap[k].find(block) == kernelMap[k].end())
             {
-                //this block is in the kernel we care about
-                lastblock[kernel] = block;
-                //first we ask are we supposed be in a sub kernel
-                //or should we be changing kernel contexts
-                if (currentKernel[kernel] == "-1" || kernelMap[currentKernel[kernel]].find(block) == kernelMap[currentKernel[kernel]].end())
-                {
-                    //if so we will set our new context
-                    string innerKernel = "-1";
-                    for (auto &[k, s] : kernelMap)
-                    {
-                        if (s.find(block) != s.end())
-                        {
-                            //we found a kernel that contains the block, but we now need to check that it is a child
-                            if (parentMap[k].find(kernel) != parentMap[k].end())
-                            {
-                                //valid child
-                                innerKernel = k;
-                                break;
-                            }
-                        }
-                    }
-                    currentKernel[kernel] = innerKernel;
-                    if (innerKernel != "-1")
-                    {
-                        currentUid[kernel] = UID[kernel];
-                        kernelIdMap[kernel][UID[kernel]++] = currentKernel[kernel];
-                    }
-                }
+                //we have left the kernel
+                kernelQueue.pop();
+                k = kernelQueue.back().name;
             }
             else
             {
-                //it isn't and we should post results
-                //cout << "hi";
+                //we have entered a nested kernel
+                ck = k;
+                kernelQueue.push(UIDStruct(ck));
             }
         }
     }
     else if (key == "LoadAddress")
     {
         uint64_t address = stoul(value, nullptr, 0);
-        for (const auto &kernel : kernels)
-        {
-            if (kernelMap[kernel].find(block) != kernelMap[kernel].end())
-            {
-                int prodUid = writeMap[kernel][address];
-                if (prodUid != -1 && prodUid != currentUid[kernel])
-                {
-                    consumerMap[kernel][currentUid[kernel]].insert(prodUid);
-                }
-                else
-                {
-                }
-            }
-        }
+        readMap[kernelQueue.back().UID].insert(writeMap[address]);
     }
     else if (key == "StoreAddress")
     {
         uint64_t address = stoul(value, nullptr, 0);
-        for (const auto &kernel : kernels)
-        {
-            if (kernelMap[kernel].find(block) != kernelMap[kernel].end())
-            {
-                writeMap[kernel][address] = currentUid[kernel];
-            }
-            else
-            {
-            }
-        }
+        writeMap[address] = kernelQueue.back().UID;
     }
 }
 
@@ -172,35 +163,14 @@ int main(int argc, char *argv[])
     {
         string index = k;
         nlohmann::json kernel = l["Blocks"];
-        kernelMap[index] = kernel.get<set<int>>();
-        kernels.push_back(k);
-        currentUid[k] = -1;
-        currentKernel[k] = "-1";
+        kernelMap[index] = kernel.get<set<int64_t>>();
+        kernelSet.emplace_back(KernelStruct(kernelMap[index], index));
     }
-    for (auto &b : j["ValidBlocks"])
-    {
-        kernelMap["-2"].insert(b.get<int64_t>());
-    }
-    kernels.emplace_back("-2");
+    kernelSet.emplace_back(KernelStruct(j["ValidBlocks"].get<set<int64_t>>(), "-1"));
+    std::sort(kernelSet.begin(), kernelSet.end(), sizeSort);
 
-    //get parent child relationships
-    for (const auto &kernel : kernelMap)
-    {
-        for (const auto &kernel2 : kernelMap)
-        {
-            if (kernel.first == kernel2.first)
-            {
-                continue;
-            }
-            vector<int> intSet;
-            set_difference(kernel.second.begin(), kernel.second.end(), kernel2.second.begin(), kernel2.second.end(), std::inserter(intSet, intSet.begin()));
-            if (intSet.empty())
-            {
-                parentMap[kernel.first].insert(kernel2.first);
-            }
-        }
-    }
-
+    ck = "-1";
+    kernelQueue.push(UIDStruct(ck));
     ProcessTrace(TraceFilename, Process, "Generating DAG", noBar);
 
     return 0;
