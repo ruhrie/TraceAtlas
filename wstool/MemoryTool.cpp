6aae4e5b183f42b86056e426022a5df8c8016650
lchang21 <lchang21@spade-11.fulton.asu.edu>
2020-04-24 09:55:02 -0700

    fix format

--- wstool/MemoryTool.cpp
+++ wstool/MemoryTool.cpp
@@ -28,8 +28,8 @@ int main(int argc, char **argv)
     uint64_t maxInput = 0;
     uint64_t maxOutput = 0;
     uint64_t maxinternal = 0;
-    //using this set of output addresses in case of duplicated
-    set<uint64_t> outputSet;
+    set<uint64_t> outputSet; //using this set of output addresses in case of duplicated
+    set<int64_t> endTimeSet; //using this set of end time to calculate the maximum internal working set
     // struct of internal working set
     for (auto it : internalAddressLivingVec)
     {
@@ -38,10 +38,6 @@ int main(int argc, char **argv)
         {
             outputSet.insert(it.address);
         }
-    }
-    set <int64_t> endTimeSet;
-    for (auto it : internalAddressLivingVec)
-    {
         endTimeSet.insert(it.deathTime);
         if (it.brithTime > *(endTimeSet.begin()))
         {
@@ -52,51 +48,6 @@ int main(int argc, char **argv)
             maxinternal = endTimeSet.size();
         }
     }
-    //time iteration
-    // for (int64_t time = 0; time < timing; time++)
-    // {
-    //     set<uint64_t> aliveInternalAddress;
-    //     // for erasing
-    //     set<int> eraseSet;
-    //     int eraseIndex = 0;
-    //     //internal virtual address iteration for each time
-    //     for (auto it : internalAddressLivingVec)
-    //     {
-    //         //the birth time of addresses is late than time now, break the loop
-    //         if (it.brithTime > time)
-    //         {
-    //             break;
-    //         }
-    //         //  brithTime < t < deathTime, it is a living internal address
-    //         if (it.brithTime <= time && it.deathTime > time)
-    //         {
-    //             aliveInternalAddress.insert(it.address);
-    //         }
-    //         //  t > deathTime, it is a died internal address, erasing this from the vector to speed up
-    //         else if (time > it.deathTime)
-    //         {
-    //             eraseSet.insert(eraseIndex);
-    //         }
-    //         eraseIndex++;
-    //     }
-    //     //erase died addresses from the vector
-    //     for (auto it : eraseSet)
-    //     {
-    //         internalAddressLivingVec.erase(internalAddressLivingVec.begin() + it);
-    //     }
-    //     //max internal size
-    //     if (maxinternal < aliveInternalAddress.size())
-    //     {
-    //         maxinternal = aliveInternalAddress.size();
-    //     }
-    //     // for debug
-    //     if (time % 1000 == 0)
-    //     {
-    //         float ProcessRatio = (float)time / WorkingSet::timing;
-    //         // for temporal debug
-    //         printf("process time :%ld, %ld, %.5f \n", time, WorkingSet::timing, ProcessRatio);
-    //     }
-    // }
     maxInput = inputMapSize;
     maxOutput = outputSet.size();
     printf("maxInput: %lu \n maxinternal: %lu \n maxOutput: %lu \n", maxInput, maxinternal, maxOutput);
--- wstool/WorkingSet.cpp
+++ wstool/WorkingSet.cpp
@@ -19,7 +19,7 @@ namespace WorkingSet
     // FirstStore is called when we know that the address appears for the first time, then:
     // 1.Update key map with the live addresses
     // 2.Construct the address structs of birth and death time
-    // Here “op” is a flag to indicate that if the address first appears from a load or a store
+    // Here “fromStore” is a flag to indicate that if the address first appears from a load or a store
     void firstStore(uint64_t addrIndex, int64_t t, bool fromStore)
     {
         // birth from a store inst
@@ -62,26 +62,6 @@ namespace WorkingSet
             inputMapSize++;
         }
     }
-
-    // LivingLoad:
-    // Update the virtual address map of the live address:
-    // The times when load instruction appears are pushed into virtual address map.
-
-    void livingLoad(uint64_t addrIndex, int64_t t)
-    {
-        //the coming address is an internal address
-        if (internalAddressIndexMap.find(addrIndex) != internalAddressIndexMap.end())
-        {
-            internalAddressLivingVec[internalAddressIndexMap[addrIndex]].deathTime = t;
-        }
-
-        if (inputAddressIndexMap.find(addrIndex) != inputAddressIndexMap.end())
-        {
-            inputAddressLivingVec[inputAddressIndexMap[addrIndex]].deathTime = t;
-        }
-        //an input address
-    }
-
     void Process(string &key, string &value)
     {
         uint64_t addressIndex;
@@ -94,9 +74,15 @@ namespace WorkingSet
         else if (key == "LoadAddress")
         {
             addressIndex = stoul(value, nullptr, 0);
-            if (internalAddressIndexMap.find(addressIndex) != internalAddressIndexMap.end() || inputAddressIndexMap.find(addressIndex) != inputAddressIndexMap.end())
+            // Update the death time in address living struct:
+            // The times when load instruction appears replaces the old death time.
+            if (internalAddressIndexMap.find(addressIndex) != internalAddressIndexMap.end())
             {
-                livingLoad(addressIndex, timing);
+                internalAddressLivingVec[internalAddressIndexMap[addressIndex]].deathTime = timing;
+            }
+            else if (inputAddressIndexMap.find(addressIndex) != inputAddressIndexMap.end())
+            {
+                inputAddressLivingVec[inputAddressIndexMap[addressIndex]].deathTime = timing;
             }
             else
             {
