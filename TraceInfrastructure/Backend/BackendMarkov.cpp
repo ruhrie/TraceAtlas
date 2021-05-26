7381f4f90875d750fc4d549bd21dbca5926b6365
benroywillis <benroywillis@gmail.com>
2021-05-26 13:10:30 -0700

    Have preliminary support for profiling using simple 2D matrix, doesn't scale

--- TraceInfrastructure/Backend/BackendMarkov.cpp
+++ TraceInfrastructure/Backend/BackendMarkov.cpp
@@ -31,13 +31,17 @@ public:
         ofstream fp(fileName, std::ios::out | std::ios::binary);
         for (auto addr1 : base)
         {
+            // first, write source node ID
+            // second, write # of outgoing edges (length of non-zero entries)
+            // third, write the sink node ID
+            // fourth, write the frequency count of the edge
             fp.write((const char *)&addr1.first, sizeof(uint64_t));
             uint64_t length = addr1.second.size();
-            fp.write((const char *)&length, sizeof(std::size_t));
+            fp.write((const char *)&length, sizeof(std::size_t)); // BW are you sure this should be size_t and not uint64_t?
             for (auto addr2 : addr1.second)
             {
                 fp.write((const char *)&addr2.first, sizeof(uint64_t));
-                fp.write((const char *)&addr2.second, sizeof(std::size_t));
+                fp.write((const char *)&addr2.second, sizeof(std::size_t)); // same with this
             }
         }
         fp.close();
@@ -77,7 +81,6 @@ public:
 };*/
 
 uint64_t b;
-uint64_t size_TAMM;
 uint64_t *markovResult;
 bool markovActive = false;
 uint64_t *TraceAtlasMarkovMap;
@@ -90,8 +93,9 @@ extern "C"
     extern uint64_t MarkovBlockCount;
     void MarkovInit(uint64_t blockCount)
     {
-        TraceAtlasMarkovMap = (uint64_t *)malloc(blockCount * sizeof(uint64_t));
-        size_TAMM = blockCount;
+        TraceAtlasMarkovMap = (uint64_t *)malloc(blockCount * blockCount * sizeof(uint64_t));
+        memset(TraceAtlasMarkovMap, 0, blockCount*blockCount*sizeof(uint64_t));
+        MarkovBlockCount = blockCount;
         markovActive = true;
     }
     void MarkovDestroy()
@@ -106,7 +110,30 @@ extern "C"
         {
             f = fopen(p, "wb");
         }
-        fwrite(TraceAtlasMarkovMap, sizeof(uint64_t) * size_TAMM, 1, f);
+        for( uint64_t i = 0; i < MarkovBlockCount; i++ )
+        {
+            // first, write the row index (source node identifier)
+            fwrite(&i, sizeof(uint64_t), 1, f);
+            uint64_t l = 0;
+            uint64_t nonZeroEntryIndices[MarkovBlockCount];
+            for( uint64_t j = 0; j < MarkovBlockCount; j++ )
+            {
+                if( *(TraceAtlasMarkovMap + (MarkovBlockCount*i) + j ) > 0 )
+                {
+                    nonZeroEntryIndices[l] = j;
+                    l++;
+                }
+            }
+            // second, write the number of sink nodes this source node has (the number of non-zero entries in the matrix)
+            fwrite(&l, sizeof(uint64_t), 1, f);
+            for( uint64_t j = 0; j < l; j++ )
+            {
+                // third, write the column value (the sink node value, which is a non-zero entry in the matrix)
+                fwrite(&nonZeroEntryIndices[j], sizeof(uint64_t), 1, f);
+                // fourth, write the entry of the matrix at size*i+j (the frequency count of that edge)
+                fwrite(TraceAtlasMarkovMap + MarkovBlockCount*i + nonZeroEntryIndices[j], sizeof(uint64_t), 1, f);
+            }
+        }
         fclose(f);
         free(TraceAtlasMarkovMap);
         markovActive = false;
@@ -118,7 +145,7 @@ extern "C"
             // this segfaults in GSL/GSL_projects_L/fft project, when processing MarkovIncrement(i64 399) (fails on the first try, preceded by 391,392,393 loop)
             // TraceAtlasMarkovMap is definitely not null at this point (shown by gdb)
             // the line that fails is in libSTL, its when two keys are being compared as equal, x = 398, y=<error reading variable>
-            TraceAtlasMarkovMap[a]++;
+            TraceAtlasMarkovMap[ MarkovBlockCount*b + a ]++;
             b = a;
             /*if (!labelList.empty())
             {
